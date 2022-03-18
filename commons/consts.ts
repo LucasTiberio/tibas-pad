429507f53a2cc984e53d501eb2aff9fa20254c68
Lucas Tiberio <ltiberio55@gmail.com>
2022-03-18 18:51:28 -0300

    fixed envs

--- commons/consts.ts
+++ commons/consts.ts
@@ -1,2 +1,2 @@
-export const MONGO_URI = process.env.MONGO_URI || ''
-export const API_ENDPOINT = process.env.API_ENDPOINT || ''
+export const MONGO_URI = process.env.NEXT_PUBLIC_MONGO_URI || ''
+export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT || ''
