4488d84568659a311dc93651bc5b4e78bab48a18
Lucas Tiberio <ltiberio55@gmail.com>
2022-03-18 20:20:20 -0300

    updated ios icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
 
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/logo.png" />
+          <link rel="apple-touch-icon" href="/logo-coruja.png" />
           <meta name="theme-color" content="#999" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
